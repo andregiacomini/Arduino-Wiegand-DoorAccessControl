f14bd5ac91d7ab199bd464fc1328d3f7e632af4e
root <root@razm-laptop>
2017-08-05 22:02:27 -0500

    added debounce to button

--- WiegandDoorAccessControl/WiegandDoorAccessControl.ino
+++ WiegandDoorAccessControl/WiegandDoorAccessControl.ino
@@ -18,6 +18,14 @@ int cardReaderBuzzer = 9;
 int cardReaderLED = 10;
 int lock = 11;
 long int count;
+int buttonState;             // the current reading from the input pin
+int lastButtonState = HIGH;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 400;    // the debounce time; increase if the output flickers
+
 
 void checkButton();
 void checkCardReader();
@@ -52,12 +60,36 @@ void loop() {
 
 void checkButton()
 {
-  if(!digitalRead(pushButtonOpenDoor))
-  {
-    digitalWrite(lock,HIGH);
-    delay(300);
-    digitalWrite(lock,LOW);  
+  // read the state of the switch into a local variable:
+  int reading = !digitalRead(pushButtonOpenDoor);
+  
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
   }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+            digitalWrite(lock,HIGH);
+            delay(300);
+            digitalWrite(lock,LOW);  
+      }
+    }
+  }
+  lastButtonState = reading;
 }
 
 void checkCardReader()
